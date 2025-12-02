f7398fcc8f7ef473694342e0f3ae4f3c828c7b31
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-02 07:06:27 +0400

    create plus_one function for looking on stack memory

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-fn read(y: bool) {
-    if y {
-        println!("y is true!")
-    }
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
+
 fn main() {
-    let x = true;
-    read(x);
+    let n = 5;
+    let y = plus_one(n);
+    println!("The value of y is: {y}")
 }
+
